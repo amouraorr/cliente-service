1c445af288b179e0675607e9b098173116c2fa56
Anderson <anderson.napoleao@outlook.com>
2025-08-06 18:01:36 -0300

    update Dockerfile

---
+++ src/main/resources/db/migration/V10__create_clientes_table.sql
@@ -0,0 +1,11 @@
+CREATE TABLE clientes (
+                           id BIGINT PRIMARY KEY,
+                           nome VARCHAR(255),
+                           cpf VARCHAR(20) not null ,
+                           dataNascimento DATE,
+                           rua VARCHAR(255),
+                           numero VARCHAR(50),
+                           cep VARCHAR(20),
+                           cidade VARCHAR(100),
+                           estado VARCHAR(50)
+);
